d60fb2b95343604a8816529c02ae34231d2d15ea
Carloshlc007 <carloshlc@hotmail.com>
2023-10-05 12:25:30 -0300

    1
    1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks.Dataflow;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        public class Player : MonoBehaviour
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
